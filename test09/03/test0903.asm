b38a01999e436a9daf12ff875ca23ca470f54263
liangxiaobin <liangxiaobin@kaytion.com>
2022-03-02 17:21:42 +0800

    添加jcxz指令测试

---
+++ test09/03/test0903.asm
@@ -0,0 +1,15 @@
+assume cs:code
+code segment
+  start: mov ax,2000H
+         mov ds,ax
+         mov bx,0
+      s: mov ch,0
+         mov cl,[bx]
+         jcxz ok
+         inc bx
+         jmp short s
+      ok: mov dx,bx
+          mov ax,4c00h
+          int 21h
+code ends
+end start
